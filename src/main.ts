b8a60b626b147eea8074a40cbe486746ef847e03
Nathan Mandreza <nymandreza@gmail.com>
2024-08-19 15:17:21 -0700

    Update initial setTimeout to 2s to give UI time to set event listener

--- src/main.ts
+++ src/main.ts
@@ -2,57 +2,7 @@ import { on, showUI } from '@create-figma-plugin/utilities'
 import { ResizeWindowHandler } from './types'
 
 export default async function () {
-  on<ResizeWindowHandler>(
-    'RESIZE_WINDOW',
-    function (windowSize: { width: number; height: number }) {
-      const { width, height } = windowSize
-      figma.ui.resize(width, height)
-    }
-  )
-
-  // Show hosted UI
-  figma.showUI(
-    `<script>window.location.href = "https://turo-bynder-assets.vercel.app"</script>`,
-    {height: 640, width: 440}
-  )
-
-  // Save access token to client storage when we receive it
-  figma.ui.onmessage = async (message) => {
-    if (message.message === 'SAVE_ACCESS_TOKEN') {
-      await figma.clientStorage.setAsync(
-        'ACCESS_TOKEN',
-        message.accessToken
-      );
-
-      console.log(message)
-    }
-  };
-
-  // Get the existing access token if we already have it
-  const accessToken = await figma.clientStorage.getAsync('ACCESS_TOKEN');
-
-  figma.ui.postMessage({
-    message: 'GET_EXISTING_ACCESS_TOKEN',
-    accessToken: accessToken || null
-  }); // add origin
-
-  console.log(figma.clientStorage.keysAsync())
-
-  // Inform the UI with the same message when we receive a new token
-  figma.ui.onmessage = async (message) => {
-    if (message.message === 'SAVE_ACCESS_TOKEN') {
-      await figma.clientStorage.setAsync(
-        'ACCESS_TOKEN',
-        message.accessToken
-      );
-  
-      figma.ui.postMessage({
-        message: 'GET_EXISTING_ACCESS_TOKEN',
-        accessToken: message.accessToken
-      }); // add origin
-    }
-  };
-
+  // Function to create new node or updated selected node with image data
   function renderImage(imgData: Uint8Array) {
     let imageHash = figma.createImage(new Uint8Array(imgData)).hash
 
@@ -80,9 +30,61 @@ export default async function () {
     }
   }
 
-  figma.ui.onmessage = async (message) => {
-    if (message.message === 'IMG_DATA') {
-      renderImage(message.imgData)
+  // Window resize handler (doesn't work because UI is hosted elsewhere)
+  on<ResizeWindowHandler>(
+    'RESIZE_WINDOW',
+    function (windowSize: { width: number; height: number }) {
+      const { width, height } = windowSize
+      figma.ui.resize(width, height)
     }
-  }
+  )
+
+  // Show hosted UI
+  figma.showUI(
+    `<script>window.location.href = "https://turo-bynder-assets.vercel.app"</script>`,
+    {height: 640, width: 440}
+  )
+
+
+  figma.ui.onmessage = async (message) => {
+    console.log("Received message from UI:", message)
+
+    switch (message.message) {
+      case 'SAVE_ACCESS_TOKEN':
+        // Save access token to client storage when we receive it
+        await figma.clientStorage.setAsync(
+          'ACCESS_TOKEN',
+          message.accessToken
+        );
+
+        // Inform the UI with the same message when we receive a new token
+        figma.ui.postMessage({
+          message: 'GET_EXISTING_ACCESS_TOKEN',
+          accessToken: message.accessToken
+        }, {
+          origin: "https://turo-bynder-assets.vercel.app"
+        });
+        break;
+      
+      case 'IMG_DATA':
+        renderImage(message.imgData)
+        break;
+      
+      default:
+        console.log("Unhandled message type:", message.message)
+    }
+  };
+
+  // Get the existing access token if we already have it
+  const accessToken = await figma.clientStorage.getAsync('ACCESS_TOKEN');
+  console.log("Existing access token in clientStorage:", accessToken)
+
+  setTimeout(() => {
+    figma.ui.postMessage({
+      message: 'GET_EXISTING_ACCESS_TOKEN',
+      accessToken: accessToken || null
+    }, {
+      origin: "https://turo-bynder-assets.vercel.app"
+    });
+  }, 2000)
 }
